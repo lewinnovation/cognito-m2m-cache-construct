03786378fda93c107c90503ef600e55bc2fdaaa9
Vidur Butalia <vidurbutalia@gmail.com>
2025-07-04 20:51:26 +0530

    feat: v1

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,46 @@
-// import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration } from "aws-cdk-lib";
+import * as api_gateway from "aws-cdk-lib/aws-apigateway";
+import { Construct } from "constructs";
 
 export interface CognitoM2MCacheConstructProps {
-  // Define construct properties here
+  tokenUrl: string;
+  cacheDuration: Duration;
+  name: string;
 }
 
 export class CognitoM2MCacheConstruct extends Construct {
+  public readonly api: api_gateway.RestApi;
+  public readonly tokenUrl: string;
 
-  constructor(scope: Construct, id: string, props: CognitoM2MCacheConstructProps = {}) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: CognitoM2MCacheConstructProps,
+  ) {
     super(scope, id);
 
-    // Define construct contents here
+    this.api = new api_gateway.RestApi(this, "CognitoM2MCacheApi", {
+      restApiName: props.name,
+      description: "Simple cache for Cognito M2M tokens",
+      deployOptions: {
+        stageName: "oauth2",
+        cachingEnabled: true,
+        cacheDataEncrypted: true,
+        cacheTtl: props.cacheDuration,
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: api_gateway.Cors.ALL_ORIGINS,
+      },
+      defaultMethodOptions: {
+        authorizationType: api_gateway.AuthorizationType.NONE,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CognitoM2MCacheConstructQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const tokenResource = this.api.root.addResource("token");
+    tokenResource.addMethod(
+      "POST",
+      new api_gateway.HttpIntegration(props.tokenUrl),
+    );
+    this.tokenUrl = this.api.urlForPath(tokenResource.path);
   }
 }
